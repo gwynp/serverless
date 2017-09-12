04ba01d41b6c436e05fd2a6d97b5d488e57fd542
Gwyn <gwyn@gwynprice.ca>
2017-09-12 15:56:26 -0400

    Added sns notifications

--- upload-serverless-lambda.py
+++ upload-serverless-lambda.py
@@ -4,15 +4,22 @@ import StringIO
 import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
-serverless_bucket= s3.Bucket('gwyn-serverless')
-serverless_zip_bucket = s3.Bucket('gwyn-serverless-zip')
+def lambda_handler(event, context):
 
-serverless_zip = StringIO.StringIO()
-serverless_zip_bucket.download_fileobj('serverless.zip', serverless_zip)
+    sns = boto3.resource('sns')
+    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+    topic = sns.Topic('arn:aws:sns:us-east-1:455432667168:deployServerlessTopic')
+    serverless_bucket= s3.Bucket('gwyn-serverless')
+    serverless_zip_bucket = s3.Bucket('gwyn-serverless-zip')
 
-with zipfile.ZipFile(serverless_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        serverless_bucket.upload_fileobj(obj,nm,ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        serverless_bucket.Object(nm).Acl().put(ACL='public-read')
+    serverless_zip = StringIO.StringIO()
+    serverless_zip_bucket.download_fileobj('serverless.zip', serverless_zip)
+
+    with zipfile.ZipFile(serverless_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            serverless_bucket.upload_fileobj(obj,nm,ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+            serverless_bucket.Object(nm).Acl().put(ACL='public-read')
+    topic.publish(Subject="Serverless Deploy", Message="Deploy Complete")
+    return 'Serverless Deploy Complete!'
+    
