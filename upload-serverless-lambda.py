41095f9fbf0dc71d44ee0355a5b3036d329d16fb
Gwyn <gwyn@gwynprice.ca>
2017-09-13 11:15:39 -0400

    Changed to use ENV vars

--- upload-serverless-lambda.py
+++ upload-serverless-lambda.py
@@ -3,17 +3,35 @@ from botocore.client import Config
 import StringIO
 import zipfile
 import mimetypes
+import os
+
 
 def lambda_handler(event, context):
-
+    SNS_TOPIC = os.environ.get("SNS_TOPIC")
+    ZIPFILE_NAME = os.environ.get("ZIPFILE_NAME")
+    BUCKET_NAME = os.environ.get("BUCKET_NAME")
     sns = boto3.resource('sns')
+    topic = sns.Topic(SNS_TOPIC)
+    location = {
+        "bucketName": BUCKET_NAME,
+        "objectKey": ZIPFILE_NAME
+    }
+    job = event.get('CodePipeline.job')
+
     s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
-    topic = sns.Topic('arn:aws:sns:us-east-1:455432667168:deployServerlessTopic')
+
+    if job:
+        for artifact in job['data']['inputArtifacts']:
+            if artifact['name'] == 'MyAppBuild':
+                location = artifact['location']['s3Location']
+
+    print "Building from " + str(location)
+
     serverless_bucket= s3.Bucket('gwyn-serverless')
-    serverless_zip_bucket = s3.Bucket('gwyn-serverless-zip')
+    serverless_zip_bucket = s3.Bucket(location['bucketName'])
 
     serverless_zip = StringIO.StringIO()
-    serverless_zip_bucket.download_fileobj('serverless.zip', serverless_zip)
+    serverless_zip_bucket.download_fileobj(location['objectKey'], serverless_zip)
 
     with zipfile.ZipFile(serverless_zip) as myzip:
         for nm in myzip.namelist():
@@ -21,5 +39,8 @@ def lambda_handler(event, context):
             serverless_bucket.upload_fileobj(obj,nm,ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
             serverless_bucket.Object(nm).Acl().put(ACL='public-read')
     topic.publish(Subject="Serverless Deploy", Message="Deploy Complete")
+
+    if job:
+        codepipeline = boto3.client('codepipeline')
+        codepipeline.put_job_success_result(jobId=job['id'])
     return 'Serverless Deploy Complete!'
-    
